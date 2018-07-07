c072bba43c86d677d43569ad4692fa624b36ad09
nmiyamoto <scotsdowneroad@gmail.com>
2018-07-08 00:52:17 +0900

    Load graph file.

--- hello_ncs/hello_ncs.cpp
+++ hello_ncs/hello_ncs.cpp
@@ -16,14 +16,44 @@
 
 
 #include <stdio.h>
-#include <stdlib.h>
-
 #include "stdafx.h"
-#include <mvnc.h>
+#include "mvnc.h"
+#include <stdlib.h>
 
 // somewhat arbitrary buffer size for the device name
 #define NAME_SIZE 100
+#define GRAPH_FILE_NAME "graph"
 
+
+void* LoadFile(const char *filename, unsigned int *len) {
+  void *p=NULL;
+
+  unsigned int filesize;
+  //open the file
+  FILE *fpr;
+  errno_t error;
+  error = fopen_s(&fpr,filename ,"rb");
+  if (error != 0) {
+    
+    printf("faild to open file.\n");
+    return p;
+  }
+  
+  if (fpr == NULL){
+    printf("error rb");
+    return p;
+  }
+  // read the file length
+  fseek(fpr, 0L, SEEK_END);
+  filesize = ftell(fpr);
+  fseek(fpr, 0L, SEEK_SET);
+
+  p = malloc(sizeof(char) * filesize);
+  if (p == NULL) return p;
+  *len = filesize;
+  fread(p, sizeof(char), filesize, fpr);
+  return p;
+}
 
 int main(int argc, char** argv)
 {
@@ -35,7 +65,6 @@ int main(int argc, char** argv)
     {   // failed to get device name, maybe none plugged in.
         printf("Error - No NCS devices found.\n");
 	      printf("    mvncStatus value: %d\n", retCode);
-        //exit(-1);
 		return 0;
     }
 
@@ -52,16 +81,43 @@ int main(int argc, char** argv)
     // Pass it to other NC API calls as needed and close it when finished.
     printf("Hello NCS! Device opened normally.\n");
 
+
+    // Now read in a graph file so graphFileBuf will point to the 
+    // bytes of the file in a memory buffer and graphFileLen will be
+    // set to the number of bytes in the graph file and memory buffer.
+    unsigned int graphFileLen;
+    void* graphFileBuf = LoadFile(GRAPH_FILE_NAME, &graphFileLen);
+    if (graphFileBuf == NULL) {
+      printf("Failed to load fraph\n");
+      return 0;
+    }
+
+    // Allocate the graph
+    void* graphHandle;
+    retCode = mvncAllocateGraph(deviceHandle, &graphHandle, graphFileBuf, graphFileLen);
+    if (retCode != MVNC_OK)
+    {   // Error allocating graph
+      printf("Could not allocate graph for file: %s\n", GRAPH_FILE_NAME);
+    }
+    else
+    {   // Successfully allocated graph. Now graphHandle is ready to go.  
+        // Use graphHandle for other API calls and call mvncDeallocateGraph
+        // when done with it.
+      printf("Successfully allocated graph for %s\n", GRAPH_FILE_NAME);
+
+      retCode = mvncDeallocateGraph(graphHandle);
+      graphHandle = NULL;
+    }
+
     retCode = mvncCloseDevice(deviceHandle);
     deviceHandle = NULL;
     if (retCode != MVNC_OK)
     {
         printf("Error - Could not close NCS device.\n");
-		printf("    mvncStatus value: %d\n", retCode);
-		//exit(-1);
-		return 0;
+    		printf("    mvncStatus value: %d\n", retCode);
+		    return 0;
     }
     printf("Goodbye NCS!  Device Closed normally.\n");
     printf("NCS device working.\n");
-	while (1);
+    return 0;
 }
